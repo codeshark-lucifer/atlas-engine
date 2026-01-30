8d9f1b33dc78c3006994588eb79269d35eb67759
codeshark-lucifer <codeshark.dev@gmail.com>
2026-01-30 16:42:04 +0700

    Update CMAKE

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/Programing Education/atlas - engine/build/external/SDL/cmake_install.cmake")
+  include("D:/Programing Education/atlas - engine/build/vendor/SDL/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
